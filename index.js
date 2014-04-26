74044d37d19263db1bce84a01e579edeb1521270
jessmchang <jessmchang@gmail.com>
2014-04-26 01:45:12 -0700

    Back-end done for the most part.

---
+++ index.js
@@ -0,0 +1,49 @@
+function fillHTML(id, title, response){
+	if(response.items){
+		var frame="<iframe width='640' height='385' src='http://www.youtube.com/embed/"+id+"'frameborder='0' allowfullscreen type='text/html'></iframe>";
+		$("#title").html(title);
+		$("#video").html(frame);
+		// final="<div id='title'>"+title+"</div><div id='video'>"+frame+"</div>";
+	}
+	else{
+		$("#video").html("No Video");
+		// final = "<div id='no'>No Video</div>"
+	}
+}
+
+var handleData = function(response) {
+	var videoId = 0;
+	var videoTitle = 0;
+	if(response.items) {
+		var videoIndex = 0;
+		while(response.items[videoIndex].id.kind != "youtube#video"){
+			videoIndex++;
+		}
+		var item = response.items[videoIndex];
+		videoId = item.id.videoId;
+		videoTitle=item.snippet.title;
+	}
+	fillHTML(videoId, videoTitle, response);
+}
+
+
+var getData = function(){
+	var keyword = encodeURIComponent($(this).val());
+	var gAPI='https://www.googleapis.com/youtube/v3/search?part=snippet&q='+keyword+'&key=AIzaSyCyl4ObA4rSynwHIWd3k1Gr5bDRXnkYe1U';	
+	var xhr;
+	if(xhr && xhr.readyState != 4){
+        xhr.abort();
+    }
+	xhr = $.ajax
+	({
+		type: "GET",
+		url: gAPI,
+		dataType:"jsonp",
+		success: handleData
+	});
+}
+
+$(document).ready(function() {
+	getData();
+	$(".search_box").keyup(getData);
+});
