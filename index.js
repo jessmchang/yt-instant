29933da62393b9ff0e1c68c9c9362d035d39df9f
jessmchang <jessmchang@gmail.com>
2014-04-27 16:36:08 -0700

    Comments bug

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ var handleComments = function(response) {
 	var i = 0;
 	if(response.feed){
 		comments = response.feed.entry;
-		console.log(comments);
+		console.log(comments[0]);
 		while(i < 5){
 			individualComment = comments[i];
 			commentAuthor = individualComment.author[0].name.$t;
