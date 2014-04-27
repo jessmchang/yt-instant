a755be4a54d33d17984984b5e580143fab75aa41
jessmchang <jessmchang@gmail.com>
2014-04-26 23:50:57 -0700

    Testing response items for comments.

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var handleData = function(response) {
 }
 
 var handleComments = function(response) {
-	console.log(response.feed);
+	console.log(response.feed.entry);
 }
 
 performAJAX = function(url, handleType){
