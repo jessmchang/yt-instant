75ca76f8a246811909a260bed7150321cb8dafac
jessmchang <jessmchang@gmail.com>
2014-04-26 23:35:57 -0700

    Added another AJAX call because different Youtube API version

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+var videoId = 0;
+var commentAPI;
+
 function fillHTML(id, title, response){
 	if(response.items){
 		var frame="<iframe width='640' height='385' src='http://www.youtube.com/embed/"+id+"'frameborder='0' allowfullscreen type='text/html'></iframe>";
 		$("#title").html(title);
 		$("#video").html(frame);
-		
+
 		// final="<div id='title'>"+title+"</div><div id='video'>"+frame+"</div>";
 	}
 	else{
@@ -13,7 +16,6 @@ function fillHTML(id, title, response){
 }   
 
 var handleData = function(response) {
-	var videoId = 0;
 	var videoTitle = 0;
 	if(response.items) {
 		var videoIndex = 0;
@@ -23,8 +25,28 @@ var handleData = function(response) {
 		var item = response.items[videoIndex];
 		videoId = item.id.videoId;
 		videoTitle=item.snippet.title;
+		commentAPI = "http://gdata.youtube.com/feeds/api/videos/"+videoId+"/comments?prettyprint=true";
+		performAJAX(commentAPI, handleComments);
 	}
 	fillHTML(videoId, videoTitle, response);
+}
+
+var handleComments = function(response) {
+	console.log("comments handled");
+}
+
+performAJAX = function(url, handleType){
+	var xhr;
+	if(xhr && xhr.readyState != 4){
+        xhr.abort();
+    }
+	xhr = $.ajax
+	({
+		type: "GET",
+		url: url,
+		dataType:"jsonp",
+		success: handleType
+	});
 }
 
 
@@ -33,18 +55,10 @@ var getData = function(){
 	var keyword = encodeURIComponent($(this).val());
 	console.log(keyword);
 	var gAPI='https://www.googleapis.com/youtube/v3/search?part=snippet&q='+keyword+'&key=AIzaSyCyl4ObA4rSynwHIWd3k1Gr5bDRXnkYe1U';
-	// var gAPI='https://www.googleapis.com/youtube/v3/search?part=snippet&q='+keyword+'&key=AIzaSyCyGiCD-y9ym0Bw4S3a3dTOYzXxr-BPBjE';	
-	var xhr;
-	if(xhr && xhr.readyState != 4){
-        xhr.abort();
-    }
-	xhr = $.ajax
-	({
-		type: "GET",
-		url: gAPI,
-		dataType:"jsonp",
-		success: handleData
-	});
+	// var gAPI='https://www.googleapis.com/youtube/v3/search?part=snippet&q='+keyword+'&key=AIzaSyCyGiCD-y9ym0Bw4S3a3dTOYzXxr-BPBjE';
+	//Perform AJAX Call.
+	performAJAX(gAPI, handleData);
+	
 }
 
 $(document).ready(function() {
